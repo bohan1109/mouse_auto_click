a6677a73f9ff4cd6135620f4756e548ea85735a8
bohan1109 <4A790162@stust.edu.tw>
2023-01-03 14:49:14 +0800

    setting time.sleep

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def move_click():
             if run:
                 win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
                 win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0) 
-            time.sleep(0.5)
+            time.sleep(0.2)
     except KeyboardInterrupt:
         print('interrupted!')
 if __name__ == '__main__':
